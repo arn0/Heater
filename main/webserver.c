aa8b09696ff49d4b15a02f1449ed7347a2fac22e
arn0 <arnoc@xs4all.nl>
2024-03-30 16:13:24 +0100

    Slip index.html

--- main/webserver.c
+++ main/webserver.c
@@ -10,6 +10,7 @@
 #include <sys/stat.h>
 #include <esp_http_server.h>
 #include "nvs_flash.h"
+#include "esp_vfs.h"
 #include "esp_spiffs.h"
 
 #include "mount.h"
@@ -21,7 +22,29 @@
 static const char *TAG = "webserver";
 int led_state = 0;
 
-#define INDEX_HTML_PATH "/spiffs/index.html"
+/* Max length a file path can have on storage */	// FIXME: remove CONFIG_, get acurate file name length
+#define CONFIG_SPIFFS_OBJ_NAME_LEN 32
+#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + CONFIG_SPIFFS_OBJ_NAME_LEN)
+
+/* Max size of an individual file. Make sure this
+ * value is same as that set in upload_script.html */
+#define MAX_FILE_SIZE   (200*1024) // 200 KB
+#define MAX_FILE_SIZE_STR "200KB"
+
+/* Scratch buffer size */
+#define SCRATCH_BUFSIZE  8192
+
+struct file_server_data {
+    /* Base path of file storage */
+    char base_path[ESP_VFS_PATH_MAX + 1];
+
+    /* Scratch buffer for temporary storage during file transfer */
+    char scratch[SCRATCH_BUFSIZE];
+};
+
+const char *base_path = "/data";
+
+#define INDEX_HTML_PATH "/data/index.html"
 char index_html[4096];
 char response_data[4096];
 
@@ -212,7 +235,6 @@ static esp_err_t get_ws_handler( httpd_req_t *req )
 {
 	if ( req->method == HTTP_GET ) {
 		ESP_LOGI( TAG, "Handshake done, the new connection was opened" );
-		//xTaskCreate( ws_task, "ws task", 4096, NULL, 5, NULL );
 
 		if ( websock_client.handle == NULL ) {
 			// We didn't have a client before, now we do. Set up notification so
@@ -295,25 +317,17 @@ static esp_err_t get_ws_handler( httpd_req_t *req )
     return ret;
 }
 
-static const httpd_uri_t get_ws = {
-        .uri        = "/ws",
-        .method     = HTTP_GET,
-        .handler    = get_ws_handler,
-        .user_ctx   = NULL,
-        .is_websocket = true
-};
 
 /*
  * URI handler for main server page
  */
 
 esp_err_t get_index_handler(httpd_req_t *req)
-
 {
 	size_t readSize = 0;
-	ESP_ERROR_CHECK(httpd_resp_set_hdr(req, "cache-control", "max-age=1")); // For development
+	//ESP_ERROR_CHECK(httpd_resp_set_hdr(req, "cache-control", "max-age=1")); // For development
 	ESP_ERROR_CHECK(httpd_resp_set_type(req, "text/html"));
-	readSize = read_spiff_buffer("/spiffs/index.html");
+	readSize = read_spiff_buffer("/data/index.html");
 	ESP_ERROR_CHECK(httpd_resp_send(req, readBuf, readSize));
 
 	heater_status.web = TAR_W_FL | ONE_W_FL | TWO_W_FL;		// force update once
@@ -321,17 +335,129 @@ esp_err_t get_index_handler(httpd_req_t *req)
 	return ESP_OK;
 }
 
-httpd_uri_t get_root = {
-    .uri = "/",
-    .method = HTTP_GET,
-    .handler = get_index_handler,
-    .user_ctx = NULL};
+#define IS_FILE_EXT(filename, ext) \
+    (strcasecmp(&filename[strlen(filename) - sizeof(ext) + 1], ext) == 0)
 
-httpd_uri_t get_index = {
-    .uri = "/index.html",
-    .method = HTTP_GET,
-    .handler = get_index_handler,
-    .user_ctx = NULL};
+/* Set HTTP response content type according to file extension */
+static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filename)
+{
+    if (IS_FILE_EXT(filename, ".html")) {
+        return httpd_resp_set_type(req, "text/html");
+    } else if (IS_FILE_EXT(filename, ".css")) {
+        return httpd_resp_set_type(req, "text/css");
+    } else if (IS_FILE_EXT(filename, ".js")) {
+        return httpd_resp_set_type(req, "application/x-javascript");
+    } else if (IS_FILE_EXT(filename, ".jpeg")) {
+        return httpd_resp_set_type(req, "image/jpeg");
+    } else if (IS_FILE_EXT(filename, ".svg")) {
+        return httpd_resp_set_type(req, "image/svg+xml");
+    } else if (IS_FILE_EXT(filename, ".ico")) {
+        return httpd_resp_set_type(req, "image/x-icon");
+    }
+    /* This is a limited set only */
+    /* For any other type always set as plain text */
+    return httpd_resp_set_type(req, "text/plain");
+}
+
+/* Copies the full path into destination buffer and returns
+ * pointer to path (skipping the preceding base path) */
+static const char* get_path_from_uri(char *dest, const char *base_path, const char *uri, size_t destsize)
+{
+    const size_t base_pathlen = strlen(base_path);
+    size_t pathlen = strlen(uri);
+
+    const char *quest = strchr(uri, '?');
+    if (quest) {
+        pathlen = MIN(pathlen, quest - uri);
+    }
+    const char *hash = strchr(uri, '#');
+    if (hash) {
+        pathlen = MIN(pathlen, hash - uri);
+    }
+
+    if (base_pathlen + pathlen + 1 > destsize) {
+        /* Full path string won't fit into destination buffer */
+        return NULL;
+    }
+
+    /* Construct full path (base + path) */
+    strcpy(dest, base_path);
+    strlcpy(dest + base_pathlen, uri, pathlen + 1);
+
+    /* Return pointer to path, skipping the base */
+    return dest + base_pathlen;
+}
+
+/* Handler to download a file kept on the server */
+static esp_err_t download_get_handler(httpd_req_t *req)
+{
+    char filepath[FILE_PATH_MAX];
+    FILE *fd = NULL;
+    struct stat file_stat;
+
+    const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path,
+                                             req->uri, sizeof(filepath));
+    if (!filename) {
+        ESP_LOGE(TAG, "Filename is too long");
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Filename too long");
+        return ESP_FAIL;
+    }
+
+    if (stat(filepath, &file_stat) == -1) {
+        /* If file not present on SPIFFS check if URI
+         * corresponds to one of the hardcoded paths */
+        if (strcmp(filename, "/index.html") == 0) {
+            return get_index_handler(req);
+        }
+        ESP_LOGE(TAG, "Failed to stat file : %s", filepath);
+        /* Respond with 404 Not Found */
+        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "File does not exist");
+        return ESP_FAIL;
+    }
+
+    fd = fopen(filepath, "r");
+    if (!fd) {
+        ESP_LOGE(TAG, "Failed to read existing file : %s", filepath);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "Sending file : %s (%ld bytes)...", filename, file_stat.st_size);
+    set_content_type_from_file(req, filename);
+
+    /* Retrieve the pointer to scratch buffer for temporary storage */
+    char *chunk = ((struct file_server_data *)req->user_ctx)->scratch;
+    size_t chunksize;
+    do {
+        /* Read file in chunks into the scratch buffer */
+        chunksize = fread(chunk, 1, SCRATCH_BUFSIZE, fd);
+
+        if (chunksize > 0) {
+            /* Send the buffer contents as HTTP response chunk */
+            if (httpd_resp_send_chunk(req, chunk, chunksize) != ESP_OK) {
+                fclose(fd);
+                ESP_LOGE(TAG, "File sending failed!");
+                /* Abort sending file */
+                httpd_resp_sendstr_chunk(req, NULL);
+                /* Respond with 500 Internal Server Error */
+                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
+               return ESP_FAIL;
+           }
+        }
+
+        /* Keep looping till the whole file is sent */
+    } while (chunksize != 0);
+
+    /* Close file after sending complete */
+    fclose(fd);
+    ESP_LOGI(TAG, "File sending complete");
+
+    /* Respond with an empty chunk to signal HTTP response completion */
+    httpd_resp_send_chunk(req, NULL, 0);
+    return ESP_OK;
+}
 
 /*
  * Webserver
@@ -342,18 +468,67 @@ httpd_handle_t start_webserver(void)
 	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 	httpd_handle_t server = NULL;
 
+    /* Use the URI wildcard matching function in order to
+     * allow the same handler to respond to multiple different
+     * target URIs which match the wildcard scheme */
+    config.uri_match_fn = httpd_uri_match_wildcard;
+
 	// Initialize structure tracking websocket to client
 	websock_client.handle = NULL;
 	websock_client.descriptor = 0;
 
-	ESP_ERROR_CHECK( spiffs_init( "/spiffs") );					// Initialize SPIFFS which holds the HTML/CSS/JS files we serve to client browser
+    static struct file_server_data *server_data = NULL;
+
+    if (server_data) {
+        ESP_LOGE(TAG, "File server already started");		// ESP_ERR_INVALID_STATE
+        return NULL;
+    }
+
+    /* Allocate memory for server data */
+    server_data = calloc(1, sizeof(struct file_server_data));
+    if (!server_data) {
+        ESP_LOGE(TAG, "Failed to allocate memory for server data");		// ESP_ERR_NO_MEM
+        return NULL;
+    }
+    strlcpy(server_data->base_path, base_path,
+            sizeof(server_data->base_path));
+
+	ESP_ERROR_CHECK( spiffs_init( "/data") );				// Initialize SPIFFS which holds the HTML/CSS/JS files we serve to client browser
 	
 	ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-	if (httpd_start(&server, &config) == ESP_OK) {				// Start the httpd server
+	if (httpd_start(&server, &config) == ESP_OK) {			// Start the httpd server
 		ESP_LOGI(TAG, "Registering URI handlers");
+
+		static httpd_uri_t get_index = {                    // URI handler for index
+   		    .uri = "/index.html",
+   		    .method = HTTP_GET,
+   		    .handler = get_index_handler,
+   		    .user_ctx = NULL};
 		httpd_register_uri_handler(server, &get_index);
+
+		static httpd_uri_t get_root = {                    	// URI handler for root
+   	        .uri = "/",
+           	.method = HTTP_GET,
+           	.handler = get_index_handler,
+           	.user_ctx = NULL};
 		httpd_register_uri_handler(server, &get_root);
-		httpd_register_uri_handler(server, &get_ws);			// Registering the ws handler
+
+		static const httpd_uri_t get_ws = {                 // URI handler for websocket
+            .uri        = "/ws",
+            .method     = HTTP_GET,
+            .handler    = get_ws_handler,
+            .user_ctx   = NULL,
+            .is_websocket = true};
+		httpd_register_uri_handler(server, &get_ws);
+
+		
+        httpd_uri_t file_download = {                       // URI handler for getting uploaded files
+			.uri       = "/*",                              // Match all URIs of type /path/to/file
+			.method    = HTTP_GET,
+			.handler   = download_get_handler,
+			.user_ctx  = server_data                        // Pass server data as context
+			};
+		httpd_register_uri_handler(server, &file_download);
 
 		xTaskCreate( send_sensor_update, "sensor update", 4096, NULL, WS_UPDATE_TASK_PRIORITY, NULL );
 		return server;
