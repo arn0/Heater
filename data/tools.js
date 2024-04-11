860ba4b8080a836ef6e5559841302449e184674a
arn0 <arnoc@xs4all.nl>
2024-04-11 13:14:38 +0200

    Add file upload from browser

--- data/tools.js
+++ data/tools.js
@@ -4,38 +4,89 @@ var websocket;
 var update;
 
 function initWebSocket() {
-  console.log('Trying to open a WebSocket connection...');
-  websocket = new WebSocket(gateway);
-  websocket.onopen    = onOpen;
-  websocket.onclose   = onClose;
-  websocket.onmessage = onMessage;
+	console.log('Trying to open a WebSocket connection...');
+	websocket = new WebSocket(gateway);
+	websocket.onopen    = onOpen;
+	websocket.onclose   = onClose;
+	websocket.onmessage = onMessage;
 }
 
 function onOpen(event) {
-  console.log('Connection opened');
+	console.log('Connection opened');
 }
 
 function onClose(event) {
-  console.log('Connection closed');
-  setTimeout(initWebSocket, 2000);
+	console.log('Connection closed');
+	setTimeout(initWebSocket, 2000);
 }
 
 function onMessage(event) {
-  update = JSON.parse(event.data);
-  //console.log(event.data);
+	update = JSON.parse(event.data);
+	//console.log(event.data);
 }
 
 window.addEventListener('load', onLoad);
 
 function onLoad(event) {
-  initWebSocket();
-  initButtons();
+	initWebSocket();
+	initButtons();
 }
-     
+
 function initButtons() {
-  document.getElementById('points').addEventListener('click', savePoints);
+	document.getElementById('points').addEventListener('click', savePoints);
 }
 
 function savePoints(){
-  websocket.send('SavePoints');
+	websocket.send('SavePoints');
+}
+
+function setpath() {
+	var default_path = document.getElementById("newfile").files[0].name;
+	document.getElementById("filepath").value = default_path;
+}
+function upload() {
+	var filePath = document.getElementById("filepath").value;
+	var upload_path = "/upload/" + filePath;
+	var fileInput = document.getElementById("newfile").files;
+
+	/* Max size of an individual file. Make sure this
+	 * value is same as that set in file_server.c */
+	var MAX_FILE_SIZE = 200*1024;
+	var MAX_FILE_SIZE_STR = "200KB";
+
+	if (fileInput.length == 0) {
+		alert("No file selected!");
+	} else if (filePath.length == 0) {
+		alert("File path on server is not set!");
+	} else if (filePath.indexOf(' ') >= 0) {
+		alert("File path on server cannot have spaces!");
+	} else if (filePath[filePath.length-1] == '/') {
+		alert("File name not specified after path!");
+	} else if (fileInput[0].size > 200*1024) {
+		alert("File size must be less than 200KB!");
+	} else {
+		document.getElementById("newfile").disabled = true;
+		document.getElementById("filepath").disabled = true;
+		document.getElementById("upload").disabled = true;
+
+		var file = fileInput[0];
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4) {
+				if (xhttp.status == 200) {
+					document.open();
+					document.write(xhttp.responseText);
+					document.close();
+				} else if (xhttp.status == 0) {
+					alert("Server closed the connection abruptly!");
+					location.reload()
+				} else {
+					alert(xhttp.status + " Error!\n" + xhttp.responseText);
+					location.reload()
+				}
+			}
+		};
+		xhttp.open("POST", upload_path, true);
+		xhttp.send(file);
+	}
 }
--- main/webserver.c
+++ main/webserver.c
@@ -37,11 +37,11 @@ int led_state = 0;
 #define SCRATCH_BUFSIZE  8192
 
 struct file_server_data {
-    /* Base path of file storage */
-    char base_path[ESP_VFS_PATH_MAX + 1];
+	 /* Base path of file storage */
+	 char base_path[ESP_VFS_PATH_MAX + 1];
 
-    /* Scratch buffer for temporary storage during file transfer */
-    char scratch[SCRATCH_BUFSIZE];
+	 /* Scratch buffer for temporary storage during file transfer */
+	 char scratch[SCRATCH_BUFSIZE];
 };
 
 const char *base_path = "/data";
@@ -268,9 +268,9 @@ static esp_err_t get_ws_handler( httpd_req_t *req )
 				}
 				break;
 		}
-   }
-   free(buf);
-   return ret;
+	}
+	free(buf);
+	return ret;
 }
 
 
@@ -292,127 +292,238 @@ esp_err_t get_index_handler(httpd_req_t *req)
 }
 
 #define IS_FILE_EXT(filename, ext) \
-    (strcasecmp(&filename[strlen(filename) - sizeof(ext) + 1], ext) == 0)
+	 (strcasecmp(&filename[strlen(filename) - sizeof(ext) + 1], ext) == 0)
 
 /* Set HTTP response content type according to file extension */
 static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filename)
 {
-    if (IS_FILE_EXT(filename, ".html")) {
-        return httpd_resp_set_type(req, "text/html");
-    } else if (IS_FILE_EXT(filename, ".css")) {
-        return httpd_resp_set_type(req, "text/css");
-    } else if (IS_FILE_EXT(filename, ".js")) {
-        return httpd_resp_set_type(req, "application/x-javascript");
-    } else if (IS_FILE_EXT(filename, ".jpeg")) {
-        return httpd_resp_set_type(req, "image/jpeg");
-    } else if (IS_FILE_EXT(filename, ".svg")) {
-        return httpd_resp_set_type(req, "image/svg+xml");
-    } else if (IS_FILE_EXT(filename, ".ico")) {
-        return httpd_resp_set_type(req, "image/x-icon");
-    }
-    /* This is a limited set only */
-    /* For any other type always set as plain text */
-    return httpd_resp_set_type(req, "text/plain");
+	 if (IS_FILE_EXT(filename, ".html")) {
+		  return httpd_resp_set_type(req, "text/html");
+	 } else if (IS_FILE_EXT(filename, ".css")) {
+		  return httpd_resp_set_type(req, "text/css");
+	 } else if (IS_FILE_EXT(filename, ".js")) {
+		  return httpd_resp_set_type(req, "application/x-javascript");
+	 } else if (IS_FILE_EXT(filename, ".jpeg")) {
+		  return httpd_resp_set_type(req, "image/jpeg");
+	 } else if (IS_FILE_EXT(filename, ".svg")) {
+		  return httpd_resp_set_type(req, "image/svg+xml");
+	 } else if (IS_FILE_EXT(filename, ".ico")) {
+		  return httpd_resp_set_type(req, "image/x-icon");
+	 }
+	 /* This is a limited set only */
+	 /* For any other type always set as plain text */
+	 return httpd_resp_set_type(req, "text/plain");
 }
 
 /* Copies the full path into destination buffer and returns
  * pointer to path (skipping the preceding base path) */
 static const char* get_path_from_uri(char *dest, const char *base_path, const char *uri, size_t destsize)
 {
-    const size_t base_pathlen = strlen(base_path);
-    size_t pathlen = strlen(uri);
+	 const size_t base_pathlen = strlen(base_path);
+	 size_t pathlen = strlen(uri);
 
-    const char *quest = strchr(uri, '?');
-    if (quest) {
-        pathlen = MIN(pathlen, quest - uri);
-    }
-    const char *hash = strchr(uri, '#');
-    if (hash) {
-        pathlen = MIN(pathlen, hash - uri);
-    }
+	 const char *quest = strchr(uri, '?');
+	 if (quest) {
+		  pathlen = MIN(pathlen, quest - uri);
+	 }
+	 const char *hash = strchr(uri, '#');
+	 if (hash) {
+		  pathlen = MIN(pathlen, hash - uri);
+	 }
 
-    if (base_pathlen + pathlen + 1 > destsize) {
-        /* Full path string won't fit into destination buffer */
-        return NULL;
-    }
+	 if (base_pathlen + pathlen + 1 > destsize) {
+		  /* Full path string won't fit into destination buffer */
+		  return NULL;
+	 }
 
-    /* Construct full path (base + path) */
-    strcpy(dest, base_path);
-    strlcpy(dest + base_pathlen, uri, pathlen + 1);
+	 /* Construct full path (base + path) */
+	 strcpy(dest, base_path);
+	 strlcpy(dest + base_pathlen, uri, pathlen + 1);
 
-    /* Return pointer to path, skipping the base */
-    return dest + base_pathlen;
+	 /* Return pointer to path, skipping the base */
+	 return dest + base_pathlen;
+}
+
+/* Handler to upload a file onto the server */
+static esp_err_t upload_post_handler(httpd_req_t *req)
+{
+	 char filepath[FILE_PATH_MAX];
+	 FILE *fd = NULL;
+	 struct stat file_stat;
+
+	 /* Skip leading "/upload" from URI to get filename */
+	 /* Note sizeof() counts NULL termination hence the -1 */
+	 const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path,
+															req->uri + sizeof("/upload") - 1, sizeof(filepath));
+	 if (!filename) {
+		  /* Respond with 500 Internal Server Error */
+		  httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Filename too long");
+		  return ESP_FAIL;
+	 }
+
+	 /* Filename cannot have a trailing '/' */
+	 if (filename[strlen(filename) - 1] == '/') {
+		  ESP_LOGE(TAG, "Invalid filename : %s", filename);
+		  httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Invalid filename");
+		  return ESP_FAIL;
+	 }
+
+	 if (stat(filepath, &file_stat) == 0) {
+		  ESP_LOGE(TAG, "File already exists : %s", filepath);
+		  /* Respond with 400 Bad Request */
+		  httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "File already exists");
+		  return ESP_FAIL;
+	 }
+
+	 /* File cannot be larger than a limit */
+	 if (req->content_len > MAX_FILE_SIZE) {
+		  ESP_LOGE(TAG, "File too large : %d bytes", req->content_len);
+		  /* Respond with 400 Bad Request */
+		  httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST,
+									 "File size must be less than "
+									 MAX_FILE_SIZE_STR "!");
+		  /* Return failure to close underlying connection else the
+			* incoming file content will keep the socket busy */
+		  return ESP_FAIL;
+	 }
+
+	 fd = fopen(filepath, "w");
+	 if (!fd) {
+		  ESP_LOGE(TAG, "Failed to create file : %s", filepath);
+		  /* Respond with 500 Internal Server Error */
+		  httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to create file");
+		  return ESP_FAIL;
+	 }
+
+	 ESP_LOGI(TAG, "Receiving file : %s...", filename);
+
+	 /* Retrieve the pointer to scratch buffer for temporary storage */
+	 char *buf = ((struct file_server_data *)req->user_ctx)->scratch;
+	 int received;
+
+	 /* Content length of the request gives
+	  * the size of the file being uploaded */
+	 int remaining = req->content_len;
+
+	 while (remaining > 0) {
+
+		  ESP_LOGI(TAG, "Remaining size : %d", remaining);
+		  /* Receive the file part by part into a buffer */
+		  if ((received = httpd_req_recv(req, buf, MIN(remaining, SCRATCH_BUFSIZE))) <= 0) {
+				if (received == HTTPD_SOCK_ERR_TIMEOUT) {
+					 /* Retry if timeout occurred */
+					 continue;
+				}
+
+				/* In case of unrecoverable error,
+				 * close and delete the unfinished file*/
+				fclose(fd);
+				unlink(filepath);
+
+				ESP_LOGE(TAG, "File reception failed!");
+				/* Respond with 500 Internal Server Error */
+				httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to receive file");
+				return ESP_FAIL;
+		  }
+
+		  /* Write buffer content to file on storage */
+		  if (received && (received != fwrite(buf, 1, received, fd))) {
+				/* Couldn't write everything to file!
+				 * Storage may be full? */
+				fclose(fd);
+				unlink(filepath);
+
+				ESP_LOGE(TAG, "File write failed!");
+				/* Respond with 500 Internal Server Error */
+				httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to write file to storage");
+				return ESP_FAIL;
+		  }
+
+		  /* Keep track of remaining size of
+			* the file left to be uploaded */
+		  remaining -= received;
+	 }
+
+	 /* Close file upon upload completion */
+	 fclose(fd);
+	 ESP_LOGI(TAG, "File reception complete");
+
+	 /* Redirect onto root to see the updated file list */
+//    httpd_resp_set_status(req, "303 See Other");
+//    httpd_resp_set_hdr(req, "Location", "/");
+//    httpd_resp_sendstr(req, "File uploaded successfully");
+	 return ESP_OK;
 }
 
 /* Handler to download a file kept on the server */
 static esp_err_t download_get_handler(httpd_req_t *req)
 {
-    char filepath[FILE_PATH_MAX];
-    FILE *fd = NULL;
-    struct stat file_stat;
+	 char filepath[FILE_PATH_MAX];
+	 FILE *fd = NULL;
+	 struct stat file_stat;
 
-    const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path,
-                                             req->uri, sizeof(filepath));
-    if (!filename) {
-        ESP_LOGE(TAG, "Filename is too long");
-        /* Respond with 500 Internal Server Error */
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Filename too long");
-        return ESP_FAIL;
-    }
+	 const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path,
+															req->uri, sizeof(filepath));
+	 if (!filename) {
+		  ESP_LOGE(TAG, "Filename is too long");
+		  /* Respond with 500 Internal Server Error */
+		  httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Filename too long");
+		  return ESP_FAIL;
+	 }
 
-    if (stat(filepath, &file_stat) == -1) {
-        /* If file not present on SPIFFS check if URI
-         * corresponds to one of the hardcoded paths */
-        if (strcmp(filename, "/index.html") == 0) {
-            return get_index_handler(req);
-        }
-        ESP_LOGE(TAG, "Failed to stat file : %s", filepath);
-        /* Respond with 404 Not Found */
-        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "File does not exist");
-        return ESP_FAIL;
-    }
+	 if (stat(filepath, &file_stat) == -1) {
+		  /* If file not present on SPIFFS check if URI
+			* corresponds to one of the hardcoded paths */
+		  if (strcmp(filename, "/index.html") == 0) {
+				return get_index_handler(req);
+		  }
+		  ESP_LOGE(TAG, "Failed to stat file : %s", filepath);
+		  /* Respond with 404 Not Found */
+		  httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "File does not exist");
+		  return ESP_FAIL;
+	 }
 
-    fd = fopen(filepath, "r");
-    if (!fd) {
-        ESP_LOGE(TAG, "Failed to read existing file : %s", filepath);
-        /* Respond with 500 Internal Server Error */
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
-        return ESP_FAIL;
-    }
+	 fd = fopen(filepath, "r");
+	 if (!fd) {
+		  ESP_LOGE(TAG, "Failed to read existing file : %s", filepath);
+		  /* Respond with 500 Internal Server Error */
+		  httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
+		  return ESP_FAIL;
+	 }
 
-    ESP_LOGI(TAG, "Sending file : %s (%ld bytes)...", filename, file_stat.st_size);
-    set_content_type_from_file(req, filename);
+	 ESP_LOGI(TAG, "Sending file : %s (%ld bytes)...", filename, file_stat.st_size);
+	 set_content_type_from_file(req, filename);
 
-    /* Retrieve the pointer to scratch buffer for temporary storage */
-    char *chunk = ((struct file_server_data *)req->user_ctx)->scratch;
-    size_t chunksize;
-    do {
-        /* Read file in chunks into the scratch buffer */
-        chunksize = fread(chunk, 1, SCRATCH_BUFSIZE, fd);
+	 /* Retrieve the pointer to scratch buffer for temporary storage */
+	 char *chunk = ((struct file_server_data *)req->user_ctx)->scratch;
+	 size_t chunksize;
+	 do {
+		  /* Read file in chunks into the scratch buffer */
+		  chunksize = fread(chunk, 1, SCRATCH_BUFSIZE, fd);
 
-        if (chunksize > 0) {
-            /* Send the buffer contents as HTTP response chunk */
-            if (httpd_resp_send_chunk(req, chunk, chunksize) != ESP_OK) {
-                fclose(fd);
-                ESP_LOGE(TAG, "File sending failed!");
-                /* Abort sending file */
-                httpd_resp_sendstr_chunk(req, NULL);
-                /* Respond with 500 Internal Server Error */
-                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
-               return ESP_FAIL;
-           }
-        }
+		  if (chunksize > 0) {
+				/* Send the buffer contents as HTTP response chunk */
+				if (httpd_resp_send_chunk(req, chunk, chunksize) != ESP_OK) {
+					 fclose(fd);
+					 ESP_LOGE(TAG, "File sending failed!");
+					 /* Abort sending file */
+					 httpd_resp_sendstr_chunk(req, NULL);
+					 /* Respond with 500 Internal Server Error */
+					 httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
+					return ESP_FAIL;
+			  }
+		  }
 
-        /* Keep looping till the whole file is sent */
-    } while (chunksize != 0);
+		  /* Keep looping till the whole file is sent */
+	 } while (chunksize != 0);
 
-    /* Close file after sending complete */
-    fclose(fd);
-    ESP_LOGI(TAG, "File sending complete");
+	 /* Close file after sending complete */
+	 fclose(fd);
+	 ESP_LOGI(TAG, "File sending complete");
 
-    /* Respond with an empty chunk to signal HTTP response completion */
-    httpd_resp_send_chunk(req, NULL, 0);
-    return ESP_OK;
+	 /* Respond with an empty chunk to signal HTTP response completion */
+	 httpd_resp_send_chunk(req, NULL, 0);
+	 return ESP_OK;
 }
 
 /*
@@ -424,64 +535,73 @@ httpd_handle_t start_webserver(void)
 	httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 	httpd_handle_t server = NULL;
 
-   /* Use the URI wildcard matching function in order to
-    * allow the same handler to respond to multiple different
-    * target URIs which match the wildcard scheme */
-   config.uri_match_fn = httpd_uri_match_wildcard;
+	/* Use the URI wildcard matching function in order to
+	 * allow the same handler to respond to multiple different
+	 * target URIs which match the wildcard scheme */
+	config.uri_match_fn = httpd_uri_match_wildcard;
 
 	// Initialize structure tracking websockets to clients
 	memset( &websock_clients, 0, sizeof(websock_clients) );
 
-   static struct file_server_data *server_data = NULL;
+	static struct file_server_data *server_data = NULL;
 
-   if (server_data) {
-      ESP_LOGE(TAG, "File server already started");		// ESP_ERR_INVALID_STATE
-      return NULL;
-   }
+	if (server_data) {
+		ESP_LOGE(TAG, "File server already started");		// ESP_ERR_INVALID_STATE
+		return NULL;
+	}
 
-   /* Allocate memory for server data */
-   server_data = calloc(1, sizeof(struct file_server_data));
-   if (!server_data) {
-      ESP_LOGE(TAG, "Failed to allocate memory for server data");		// ESP_ERR_NO_MEM
-      return NULL;
-   }
-   strlcpy(server_data->base_path, base_path,
-            sizeof(server_data->base_path));
+	/* Allocate memory for server data */
+	server_data = calloc(1, sizeof(struct file_server_data));
+	if (!server_data) {
+		ESP_LOGE(TAG, "Failed to allocate memory for server data");		// ESP_ERR_NO_MEM
+		return NULL;
+	}
+	strlcpy(server_data->base_path, base_path,
+				sizeof(server_data->base_path));
 	
 	ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
 	if (httpd_start(&server, &config) == ESP_OK) {			// Start the httpd server
 		ESP_LOGI(TAG, "Registering URI handlers");
 
 		static httpd_uri_t get_index = {                    // URI handler for index
-   		    .uri = "/index.html",
-   		    .method = HTTP_GET,
-   		    .handler = get_index_handler,
-   		    .user_ctx = NULL};
+				 .uri = "/index.html",
+				 .method = HTTP_GET,
+				 .handler = get_index_handler,
+				 .user_ctx = NULL};
 		httpd_register_uri_handler(server, &get_index);
 
 		static httpd_uri_t get_root = {                    	// URI handler for root
-   	        .uri = "/",
-           	.method = HTTP_GET,
-           	.handler = get_index_handler,
-           	.user_ctx = NULL};
+				  .uri = "/",
+			  	.method = HTTP_GET,
+			  	.handler = get_index_handler,
+			  	.user_ctx = NULL};
 		httpd_register_uri_handler(server, &get_root);
 
 		static const httpd_uri_t get_ws = {                 // URI handler for websocket
-            .uri        = "/ws",
-            .method     = HTTP_GET,
-            .handler    = get_ws_handler,
-            .user_ctx   = NULL,
-            .is_websocket = true};
+				.uri        = "/ws",
+				.method     = HTTP_GET,
+				.handler    = get_ws_handler,
+				.user_ctx   = NULL,
+				.is_websocket = true};
 		httpd_register_uri_handler(server, &get_ws);
 
 		
-        httpd_uri_t file_download = {                       // URI handler for getting uploaded files
+		  httpd_uri_t file_download = {                    // URI handler for getting uploaded files
 			.uri       = "/*",                              // Match all URIs of type /path/to/file
 			.method    = HTTP_GET,
 			.handler   = download_get_handler,
 			.user_ctx  = server_data                        // Pass server data as context
 			};
 		httpd_register_uri_handler(server, &file_download);
+
+		httpd_uri_t file_upload = {								// URI handler for uploading files to server
+
+		  .uri       = "/upload/*",								// Match all URIs of type /upload/path/to/file
+		  .method    = HTTP_POST,
+		  .handler   = upload_post_handler,
+		  .user_ctx  = server_data									// Pass server data as context
+		};
+		httpd_register_uri_handler(server, &file_upload);
 
 		xTaskCreate( send_sensor_update, "sensor update", 4096+1024, NULL, WS_UPDATE_TASK_PRIORITY, NULL );
 		return server;
